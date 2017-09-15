f4eb1bd33ca8a37ab6f2332ca8ef0c2b4911046c
Daniel Lima Oliveira <daniel102510@hotmail.com>
2017-09-15 00:00:27 -0300

    criado classe e tela de classificação parcial

--- Projeto_IA_2/src/codigos/Log.java
+++ Projeto_IA_2/src/codigos/Log.java
@@ -5,5 +5,17 @@ import java.util.Collections;
 
 public class Log {
     public static ArrayList<String> LOG = new ArrayList<>();
+ 
     
+    public static String resumo(){
+        String texto = "";
+        for (String s : Log.LOG) {
+            texto = texto + "\n" + s;
+        }
+        return texto;
+    }
+    
+    public static void limpaLog(){
+        LOG = new ArrayList<>();
+    }
 }
